ac896941f22d2022168f2433e5bcfc68d3306e33
yakov504 <yakov0546080824@gmail.com>
2025-01-14 14:12:28 +0200

    first push

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './index.css'
+
+function App() {
+
+  return (
+   <div>
+      <h1 className='max-h-40 text-red-900'>hello world</h1>
+   </div>
+  )
+}
+
+export default App

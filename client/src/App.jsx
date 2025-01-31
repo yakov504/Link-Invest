40ca36d445d798d447dcbb81705b3966ea1a3b11
yakov504 <yakov0546080824@gmail.com>
2025-01-31 17:36:42 +0200

    if loged in

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,10 @@
 import { useState, useEffect } from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
+import axios from 'axios'
+
 import './App.css';
 import './index.css';
+
 import HomePage from './components/HomePage/HomePage';
 import AgentProfile from './components/AgentProfile';
 import NavBar from './components/NavBar';
@@ -12,6 +15,8 @@ export default function App() {
   const location = useLocation(); 
   const [isScroll, setIsScroll] = useState(false);
   const [navClass, setNavClass] = useState('nav');
+  // const [ user, setUser ] = useState(null)
+  // const [ loading, setLoading ] = useState(true);
 
   useEffect(() => {
     if (location.pathname === '/HomePage') {
@@ -30,12 +35,33 @@ export default function App() {
     }
   }, [location.pathname]);
 
+  // useEffect(() => {
+  //   const fetchUserDate = async () => {
+  //     try{
+  //       const token = localStorage.getItem('teken');
+  //       if(!token){
+  //         setLoading(false)
+  //         return;
+  //       }
+  //       const res = await axios.get('http://127.0.0.1:3000/api/v1/users/logme/:name', {
+  //         headers: {Authorization: `Beares ${token}`},
+  //       })
+  //       setUser( res.data.data.user);
+  //     }catch( err ){
+  //       console.log('error fetching user',err);
+  //     } finally{
+  //       setLoading(false)
+  //     }
+  //   };
+  //   fetchUserDate();
+  // },[])
+
   return (
     <div>
       <NavBar className={navClass} />
       <Routes>
         <Route path="/Login" element={<Login />} />
-        <Route path="/AgentProfile" element={<AgentProfile />} />
+        <Route path="/AgentProfile" element={<AgentProfile/>} />
         <Route path="/HomePage" element={<HomePage />} />
         <Route path='/BuySellRent' element={<BuySellRent/>}/>
       </Routes>
--- client/src/components/AgentProfile.jsx
+++ client/src/components/AgentProfile.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import { use } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
-export default function AgentProfile() {
+export default function AgentProfile(props) {
+  const [ user, setUser ] = useState(null)
+  const [ loading, setLoading ] = useState(true);
+
+  useEffect(() => {
+    const fetchUserDate = async () => {
+      try{
+        const token = localStorage.getItem('teken');
+        if(!token){
+          setLoading(false)
+          return;
+        }
+        const res = await axios.get('http://127.0.0.1:3000/api/v1/users/logme/:name', {
+          headers: {Authorization: `Beares ${token}`},
+        })
+        setUser( res.data.data.user);
+      }catch( err ){
+        console.log('error fetching user',err);
+      } finally{
+        setLoading(false)
+      }
+    };
+    fetchUserDate();
+  },[])
+
+  if(loading) return <p>Loading...</p>
+  if(!user) return <p>you are not logged in.</p>
+
+
   return (
     <div>
-      <h1>Agent Profile</h1>
+      <h1>Welcome to your profile, {user.name}!</h1>
+      <p>Email: {user.email}</p>
+      <p>Phone Number: {user.phone_number}</p>
+      <p>Role: {user.role}</p>
     </div>
   )
 }
